b71a6bb21028fa49025e8880cd70d28156399bc4
Zucoprince <pedro.zucolo@outlook.com>
2023-11-03 16:04:48 -0300

    Adição de fraquezas e vantagensm, e feito boa parte da corrente evolutiva.

--- src/Components/SearchTypeUrl.jsx
+++ src/Components/SearchTypeUrl.jsx
@@ -1,28 +1,70 @@
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
+import GetTypeAverage from "./GetTypeAverage";
 
 export default function SearchTypeUrl(props) {
   const [urlTypeUm, setUrlTypeUm] = useState("");
   const [urlTypeDois, setUrlTypeDois] = useState("");
   const [contentUm, setContentUm] = useState([]);
   const [contentDois, setContentDois] = useState([]);
+  const typeAverageUm = GetTypeAverage(contentUm);
+  const typeAverageDois = GetTypeAverage(contentDois);
 
-  console.log(props);
+  const combinedTypeAverages = {
+    double_damage_from: [
+      ...new Set([
+        ...typeAverageUm.double_damage_from,
+        ...typeAverageDois.double_damage_from,
+      ]),
+    ],
+    half_damage_from: [
+      ...new Set([
+        ...typeAverageUm.half_damage_from,
+        ...typeAverageDois.half_damage_from,
+      ]),
+    ],
+    no_damage_from: [
+      ...new Set([
+        ...typeAverageUm.no_damage_from,
+        ...typeAverageDois.no_damage_from,
+      ]),
+    ],
+    double_damage_to: [
+      ...new Set([
+        ...typeAverageUm.double_damage_to,
+        ...typeAverageDois.double_damage_to,
+      ]),
+    ],
+    half_damage_to: [
+      ...new Set([
+        ...typeAverageUm.half_damage_to,
+        ...typeAverageDois.half_damage_to,
+      ]),
+    ],
+    no_damage_to: [
+      ...new Set([
+        ...typeAverageUm.no_damage_to,
+        ...typeAverageDois.no_damage_to,
+      ]),
+    ],
+  };
 
   useEffect(() => {
     setUrlTypeUm(props.types[0].type.url);
     if (props.types.length >= 2) {
       setUrlTypeDois(props.types[1].type.url);
     }
-  }, [setUrlTypeUm, props.types]);
+  }, [props.types]);
 
   useEffect(() => {
     const fetchPokemonSpecies = async () => {
       try {
         const responseUm = await api.get(`${urlTypeUm}`);
         setContentUm(responseUm.data);
-        const responseDois = await api.get(`${urlTypeDois}`);
-        setContentDois(responseDois.data);
+        if (urlTypeDois) {
+          const responseDois = await api.get(`${urlTypeDois}`);
+          setContentDois(responseDois.data);
+        }
       } catch (error) {
         console.error("Oops! Ocorreu um erro: " + error);
       }
@@ -30,5 +72,5 @@ export default function SearchTypeUrl(props) {
     fetchPokemonSpecies();
   }, [urlTypeUm, urlTypeDois]);
 
-  return { contentUm, contentDois }
+  return combinedTypeAverages;
 }
