9f8732f9c27711593bd28b2b17f5b7d0550d32a2
Zucoprince <pedro.zucolo@outlook.com>
2023-11-06 11:56:45 -0300

    SDKPJPSDN

---
+++ src/Components/GetPokemonChainImage.jsx
@@ -0,0 +1,164 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import "../Styles/GetPokemonChainImage.css";
+import PropTypes from "prop-types";
+import Seta from "../Images/seta-direita.png";
+
+export default function GetPokemonChainImage(props) {
+  GetPokemonChainImage.propTypes = {
+    pokemonData: PropTypes.object.isRequired,
+  };
+
+  const pokemonsSpecieData = props.pokemonData;
+  const [pokemonsId, setPokemonsId] = useState({
+    firstPoke: null,
+    secondPoke: null,
+    thirdPoke: null,
+  });
+  const [pokemonsData, setPokemonsData] = useState({
+    firstPoke: {},
+    secondPoke: {},
+    thirdPoke: {},
+  });
+  const [pokemonsImage, setPokemonsImage] = useState({
+    firstPoke: "",
+    secondPoke: "",
+    thirdPoke: "",
+  });
+
+  useEffect(() => {
+    if (pokemonsSpecieData) {
+      setPokemonsId({
+        firstPoke: pokemonsSpecieData.firstPoke
+          ? pokemonsSpecieData.firstPoke.id
+          : null,
+        secondPoke: pokemonsSpecieData.secondPoke
+          ? pokemonsSpecieData.secondPoke.id
+          : null,
+        thirdPoke: pokemonsSpecieData.thirdPoke
+          ? pokemonsSpecieData.thirdPoke.id
+          : null,
+      });
+    }
+  }, [pokemonsSpecieData]);
+
+  useEffect(() => {
+    if (pokemonsId.firstPoke !== null) {
+      const fetchPokemon = async () => {
+        try {
+          const responseFirstPoke = await api.get(
+            `pokemon/${pokemonsId.firstPoke}`
+          );
+          setPokemonsData((prevState) => ({
+            ...prevState,
+            firstPoke: responseFirstPoke.data,
+          }));
+
+          if (pokemonsId.secondPoke !== null) {
+            const responseSecondPoke = await api.get(
+              `pokemon/${pokemonsId.secondPoke}`
+            );
+            setPokemonsData((prevState) => ({
+              ...prevState,
+              secondPoke: responseSecondPoke.data,
+            }));
+          }
+
+          if (pokemonsId.thirdPoke !== null) {
+            const responseThirdPoke = await api.get(
+              `pokemon/${pokemonsId.thirdPoke}`
+            );
+            setPokemonsData((prevState) => ({
+              ...prevState,
+              thirdPoke: responseThirdPoke.data,
+            }));
+          }
+        } catch (error) {
+          console.log("Oops! Ocorreu um erro: " + error);
+        }
+      };
+
+      fetchPokemon();
+    }
+  }, [pokemonsId]);
+
+  useEffect(() => {
+    if (Object.keys(pokemonsData.firstPoke).length !== 0) {
+      setPokemonsImage((prevState) => ({
+        ...prevState,
+        firstPoke:
+          pokemonsData.firstPoke.sprites.other["official-artwork"]
+            .front_default,
+      }));
+    }
+
+    if (Object.keys(pokemonsData.secondPoke).length !== 0) {
+      setPokemonsImage((prevState) => ({
+        ...prevState,
+        secondPoke:
+          pokemonsData.secondPoke.sprites.other["official-artwork"]
+            .front_default,
+      }));
+    }
+
+    if (Object.keys(pokemonsData.thirdPoke).length !== 0) {
+      setPokemonsImage((prevState) => ({
+        ...prevState,
+        thirdPoke:
+          pokemonsData.thirdPoke.sprites.other["official-artwork"]
+            .front_default,
+      }));
+    }
+  }, [pokemonsData]);
+
+  const renderImage = () => {
+    return (
+      <div className="container_imagens">
+        {pokemonsImage.firstPoke && (
+          <div className="container_primeiro_pokemon">
+            <h3 className="nome_poke_primeiro">
+              {pokemonsData.firstPoke.name}
+            </h3>
+            <img
+              src={pokemonsImage.firstPoke}
+              alt={`Imagem do ${pokemonsData.firstPoke.name}`}
+              className="imagem_pokemon_evolução"
+            />
+          </div>
+        )}
+        {pokemonsImage.secondPoke && (
+          <>
+            <img src={Seta} alt="Seta" className="imagem_seta" />
+            <div className="container_segundo_pokemon">
+              <h3 className="nome_poke_segundo">
+                {pokemonsData.secondPoke.name}
+              </h3>
+              <img
+                src={pokemonsImage.secondPoke}
+                alt={`Imagem do ${pokemonsData.secondPoke.name}`}
+                className="imagem_pokemon_evolução"
+              />
+            </div>
+          </>
+        )}
+        {pokemonsImage.thirdPoke && (
+          <>
+            <img src={Seta} alt="Seta" className="imagem_seta" />
+            <div className="container_terceiro_pokemon">
+              <h3 className="nome_poke_terceiro">
+                {pokemonsData.thirdPoke.name}
+              </h3>
+              <img
+                src={pokemonsImage.thirdPoke}
+                alt={`Imagem do ${pokemonsData.thirdPoke.name}`}
+                className="imagem_pokemon_evolução"
+              />
+            </div>
+          </>
+        )}
+      </div>
+    );
+  };
+
+  return <>{renderImage()}</>;
+}
---
+++ src/Components/SearchEvoUrl.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import GetPokemonChainImage from "./GetPokemonChainImage";
+import PropTypes from "prop-types";
+
+export default function SearchEvoUrl(props) {
+  SearchEvoUrl.propTypes = {
+    pokemonSpecies: PropTypes.object.isRequired,
+  };
+
+  const urlEvoChain = props.pokemonSpecies.evolution_chain;
+  const [evoChain, setEvoChain] = useState([]);
+  const [pickPokemonUrl, setPickPokemonUrl] = useState({
+    firstPoke: "",
+    secondPoke: "",
+    thirdPoke: "",
+  });
+  const [pickPokemonData, setPickPokemonData] = useState([
+    {
+      firstPoke: {},
+    },
+    {
+      secondPoke: {},
+    },
+    {
+      thirdPoke: {},
+    },
+  ]);
+
+  useEffect(() => {
+    if (urlEvoChain) {
+      const fetchPokemonEvoChain = async () => {
+        try {
+          const response = await api.get(urlEvoChain.url);
+          setEvoChain(response.data.chain);
+        } catch (error) {
+          console.error("Oops! Ocorreu um erro: " + error);
+        }
+      };
+      fetchPokemonEvoChain();
+    }
+  }, [urlEvoChain]);
+
+  useEffect(() => {
+    console.log(evoChain);
+    if (evoChain && evoChain.evolves_to && evoChain.evolves_to.length > 0) {
+      const firstPoke = evoChain.species.url;
+      let secondPoke = "";
+      let thirdPoke = "";
+
+      if (
+        evoChain.evolves_to[0].evolves_to &&
+        evoChain.evolves_to[0].evolves_to.length > 0
+      ) {
+        secondPoke = evoChain.evolves_to[0].species.url;
+        thirdPoke = evoChain.evolves_to[0].evolves_to[0].species.url;
+      } else if (evoChain.evolves_to && evoChain.evolves_to.length > 0) {
+        secondPoke = evoChain.evolves_to[0].species.url;
+      }
+
+      setPickPokemonUrl({ firstPoke, secondPoke, thirdPoke });
+    }
+  }, [evoChain]);
+
+  useEffect(() => {
+    if (pickPokemonUrl.firstPoke !== "") {
+      const fetchPokemonData = async () => {
+        try {
+          const responseFirstPokemon = await api.get(pickPokemonUrl.firstPoke);
+          setPickPokemonData((prevState) => ({
+            ...prevState,
+            firstPoke: responseFirstPokemon.data,
+          }));
+          if (pickPokemonUrl.secondPoke !== "") {
+            const responseSecondPokemon = await api.get(
+              pickPokemonUrl.secondPoke
+            );
+            setPickPokemonData((prevState) => ({
+              ...prevState,
+              secondPoke: responseSecondPokemon.data,
+            }));
+          }
+
+          if (pickPokemonUrl.thirdPoke !== "") {
+            const responseThirdPokemon = await api.get(
+              pickPokemonUrl.thirdPoke
+            );
+            setPickPokemonData((prevState) => ({
+              ...prevState,
+              thirdPoke: responseThirdPokemon.data,
+            }));
+          }
+        } catch (error) {
+          console.log("Oops! Ocorreu um erro: " + error);
+        }
+      };
+
+      fetchPokemonData();
+    }
+  }, [pickPokemonUrl]);
+
+  return <GetPokemonChainImage pokemonData={pickPokemonData} />;
+}
